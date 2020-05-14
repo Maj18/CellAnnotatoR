03e59aa28474f0d3572932f077eef39ab929f629
YUAN LI <yuanli@YUANs-MacBook-Pro.local>
2020-05-14 12:22:29 +0200

    update the tfIdfOnType() function

--- R/tfidf_transformation_on_types.R
+++ R/tfidf_transformation_on_types.R
@@ -3,8 +3,9 @@
 #' @param p2 the counts object within data variable p2 (p2$counts) is what need here. counts is a sparse matrix of normalized gene expresson, col: cells, row: genes.
 #' @param ann.by.level Output from re-annotation (assignCellsByScores()); a list of 3 objects: "annotation" (this is what we need here), "scores", and "annoation.filt"; annotation is list of hierarchial levels (e.g. l1, l2, l3); at each level, there are annotation (cell types at this hierarchical level are assigned to cells) on all the studied cells.
 #' @param clf.data Output from getClassificationData(); a list of 3 objects: "cm", "classification.tree", "gene.table" and "marker.list" (this is what we need here in this function); marker.list is a list of cell types, with each type containing 3 objects: "expressed", "not_expressed" and "parent", all three objects each has a vector of markers
+#' @param topgene For each cell, the genes have been sort in a descending order regarding the TF-IDF values, topgene here decides how many top genes will be collected for calculating top5ifidf.sum.per.type.by.level.
 #' @export s.pos.v3 a list of hierarchial levels, each level is a vector of per-cell total gene expression levels.
-tfIdfOnType <- function(p2, ann.by.level, clf.data){
+tfIdfOnType <- function(p2, ann.by.level, clf.data=NULL, topgene=10){
   #Mean gene expression per cell type for each gene:
   cm <- p2$counts %>% as.matrix() #(normalized counts, row: cells, col: genes)
   ann.per.type.by.level <- ann.by.level$annotation %>% lapply(function(n) split(n,n)) # a list of hierarchial levels (e.g. l1, l2, l3; each level has its own cell types and each type has all the cells [names] that belong to it)
@@ -20,17 +21,22 @@ tfIdfOnType <- function(p2, ann.by.level, clf.data){
   for (i in names(mean.GE.per.type.per.gene)) { #i: hierarchical level
     tfidf.per.type.per.gene[[i]]<-(mean.GE.per.type.per.gene[[i]]/colsum[[i]])*log(nrow(colsum[[i]])/c*rowsum[[i]]) #The point to have c here is that the gene expression data here is normalized (between 0 and 1), not read count, so a bit deviate from the orignal tf-idf idea (where here it is the total number of documents that contain a particular word/translated to the present case: the number of cell types that contain a particular gene).
   } #tfidf.per.type.per.gene: a list of hierarchial levels, each level has a dataframe: col, cell types; row, genes.
+  if (!is.null(clf.data)) {
+    markers.per.type <- clf.data$marker.list %>% lapply(function(n) n$"expressed") #here, we only take positive markers!!!!! markers.per.type: a list of cell types, each type has positive markers.
+    names(markers.per.type)[names(markers.per.type) %in% c("0", "1")] <- c("X0", "X1") #change the cell type names, so they are the same as those in tfidf.per.type.per.gene
+    names(markers.per.type) <- gsub(" ", ".", names(markers.per.type))
 
-  markers.per.type <- clf.data$marker.list %>% lapply(function(n) n$"expressed") #here, we only take positive markers!!!!! markers.per.type: a list of cell types, each type has positive markers.
-  names(markers.per.type)[names(markers.per.type) %in% c("0", "1")] <- c("X0", "X1") #change the cell type names, so they are the same as those in tfidf.per.type.per.gene
-  names(markers.per.type) <- gsub(" ", ".", names(markers.per.type))
+    s.pos.v3 <-
+      sapply(tfidf.per.type.per.gene, function(x) #x (a hierarchical level of tfidf.per.type.per.gene): a dataframe, col, cell types; row, genes
+        mapply(function(m, n)
+          x[m, n], markers.per.type[intersect(colnames(x), names(markers.per.type))], intersect(colnames(x), names(markers.per.type)))) %>% # so far the data: a list of hierarchial levels, each levels contains a dataset: col, genes; row, cells.
+      sapply(function(n)
+        sapply(n, sum)) # here we calcualte col sum: sum of the expression of all genes within each cell.
+  }else{
+    s.pos.v3 <- NULL
+  }
 
-  s.pos.v3 <-
-    sapply(tfidf.per.type.per.gene, function(x) #x (a hierarchical level of tfidf.per.type.per.gene): a dataframe, col, cell types; row, genes
-      mapply(function(m, n)
-        x[m, n], markers.per.type[intersect(colnames(x), names(markers.per.type))], intersect(colnames(x), names(markers.per.type)))) %>% # so far the data: a list of hierarchial levels, each levels contains a dataset: col, genes; row, cells.
-    sapply(function(n)
-      sapply(n, sum)) # here we calcualte col sum: sum of the expression of all genes within each cell.
-
-  return(s.pos.v3) # a list of hierarchial levels, each level is a vector of per-cell GE sums.
+  top5ifidf.sum.per.type.by.level <- tfidf.per.type.per.gene %>% lapply(function(n) lapply(n,
+                                            function(m) head(sort(m, decreasing = T), topgene)%>%sum))
+  return(list(s.pos.v3=s.pos.v3, top5ifidf.sum.per.type.by.level=top5ifidf.sum.per.type.by.level)) # a list of hierarchial levels, each level is a vector of per-cell GE sums.
 }
