201af816d5dfc07450fa7ff8d88458271a31da0a
YUAN LI <yuanli@YUANs-MacBook-Pro.local>
2020-06-29 02:39:40 +0200

    Update getMarkerScoresPerCellType, so that the St normalization is based on cell type under the same parent rather than at the same level

--- CellAnnotatoR-dev/R/annotation.R
+++ CellAnnotatoR-dev/R/annotation.R
@@ -309,44 +309,14 @@ normalizeScores <- function(scores, min.val=1e-10) {
   return(scores)
 }
 
-#' Return initial scores of each cell type for each cell
-#'
-#' @param score.info pre-calculated score info from `getMarkerScoreInfo`
-#' @param aggr should individual gene scores be aggregated per cell type? If `FALSE`,
-#' returns list of data.frames, showing scores of each gene for each cell.
+#' Li: Return initial scores of each cell type for each cell
+#' Li: Here, the St nromalization is on all the cell types at each layer, not on cell types under the same parent.
+#' Li: Now, the St nromalization is on all the cell types under the same parent.
+#' Li: @param score.info pre-calculated score info from `getMarkerScoreInfo`
+#' Li: @param aggr should individual gene scores be aggregated per cell type? If `FALSE`,
+#' Li: returns list of data.frames, showing scores of each gene for each cell.
 #' Useful for debugging list of markers.
 #' @inheritParams getMarkerScoreInfo
-#'
-#' @return data.frame with rows corresponding to cells and columns corresponding to cell types.
-#'   Values are cell type scores, normalized per level of hierarchy
-getMarkerScoresPerCellTypeLi <- function(clf.data, score.info=NULL, aggr=T) {
-  if (is.null(score.info)) {
-    score.info <- getMarkerScoreInfo(clf.data, aggr=aggr)
-  }
-
-  scores <- lapply(score.info, `[[`, "scores")
-  if (!aggr)
-    return(lapply(scores, as.matrix) %>% lapply(as.data.frame, optional=T))
-
-  clf.nodes <- classificationTreeToDf(clf.data$classification.tree)
-  scores %<>% as.data.frame(optional=T)
-
-  parent <- split(clf.nodes$Parent, clf.nodes$PathLen)
-  node <- split(clf.nodes$Node, clf.nodes$PathLen)
-  family <- lapply(1:length(parent), function(n) split(node[[n]], parent[[n]])) %>% unlist(recursive=F)
-  
-  scores <- lapply(family, function(n) scores[,n]%<>% normalizeScores()) # normlization: types under the same parent
-  coln<- scores %>% sapply(colnames) %>% Reduce(c,.)
-  scores %<>% as.data.frame 
-  colnames(scores) <- coln
-
-  return(scores)
-}
-
-
-#' Useful for debugging list of markers.
-#' @inheritParams getMarkerScoreInfo
-#'
 #' @return data.frame with rows corresponding to cells and columns corresponding to cell types.
 #'   Values are cell type scores, normalized per level of hierarchy
 getMarkerScoresPerCellType <- function(clf.data, score.info=NULL, aggr=T) {
@@ -361,9 +331,14 @@ getMarkerScoresPerCellType <- function(clf.data, score.info=NULL, aggr=T) {
   clf.nodes <- classificationTreeToDf(clf.data$classification.tree)
   scores %<>% as.data.frame(optional=T)
   
-  for (nodes in split(clf.nodes$Node, clf.nodes$PathLen)) {
-    scores[, nodes]  %<>% normalizeScores()
-  }
+  parent <- split(clf.nodes$Parent, clf.nodes$PathLen)
+  node <- split(clf.nodes$Node, clf.nodes$PathLen)
+  family <- lapply(1:length(parent), function(n) split(node[[n]], parent[[n]])) %>% unlist(recursive=F)
+  
+  scores <- lapply(family, function(n) scores[,n]%<>% normalizeScores()) # normlization: types under the same parent
+  coln<- scores %>% sapply(colnames) %>% Reduce(c,.)
+  scores %<>% as.data.frame 
+  colnames(scores) <- coln
   
   return(scores)
 }
