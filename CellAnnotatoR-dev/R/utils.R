20fb462984cbeb3bfefb09af0bd8fa54135f5394
YUAN LI <yuanli@YUANs-MacBook-Pro.local>
2020-06-29 14:03:36 +0200

    Viktor added the classificationTreeToHierarhy function

--- CellAnnotatoR-dev/R/utils.R
+++ CellAnnotatoR-dev/R/utils.R
@@ -86,3 +86,11 @@ rateMetricsPerType <- function(ann.res, ann.reference) {
     as.data.frame()
   return(res)
 }
+
+classificationTreeToHierarhy <- function(clf.tree, max.depth=NULL) {
+  hierarchy.df <- classificationTreeToDf(clf.tree)
+  if (!is.null(max.depth)) {
+    hierarchy.df %<>% dplyr::filter(PathLen <= max.depth)
+  }
+  hierarchy.df %$% setdiff(Node, Parent) %>% mergeAnnotationByLevels(clf.tree) %>% as_tibble() %>% splitDfByNextCol()
+}
